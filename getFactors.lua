0be4394bdb8b5c2a9657d9ac764472b04f49a8ba
jz391 <128315339+jz391@users.noreply.github.com>
2023-04-10 19:04:06 -0400

    constraints added

--- getFactors.lua
+++ getFactors.lua
@@ -1,6 +1,11 @@
 local number = 84 --number to get factors of
 
 local function main()
+    if number < 0 or number%1 ~= 0 then
+        warn("Number has to be a positive integer")
+        return
+    end
+    
     local recentFactor2 = 1
     for factor1 = 2, number-1 do
         local factor2 = number/factor1
